3361d0a2a2bfc07259147336e957f5e13d50931b
ClassUser <ClassUser@spbrtk.local>
2019-10-31 14:37:50 +0300

    NOTHINGV3

--- common/views/test/index.php
+++ common/views/test/index.php
@@ -5,48 +5,73 @@
 
 <div class="body">
 
-    <div class="col-md-3 col-xs-12">
-        <div class ="x_panel">
-            <div class="info">
 
-            </div>
-            <div class="button">
+<div class="row">
+    <div class="col-md-12">
+        <div class="x_panel">
+            <div class="x_content">
+                <div class="row">
+                    <!-- --------Начало блоков---------- -->
+                    <div class="col-md-55">
+                        <div class="thumbnail">
+                            <div class="image view view-first">
 
+                            </div>
+                            <div class="caption">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- --------------Блок 1---------------- -->
+                    <div class="col-md-55">
+                        <div class="thumbnail">
+                            <div class="image view view-first">
+
+                            </div>
+                            <div class="caption">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- --------------Блок 2---------------- -->
+                    <div class="col-md-55">
+                        <div class="thumbnail">
+                            <div class="image view view-first">
+
+                            </div>
+                            <div class="caption">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- --------------Блок 3---------------- -->
+                    <div class="col-md-55">
+                        <div class="thumbnail">
+                            <div class="image view view-first">
+
+                            </div>
+                            <div class="caption">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- --------------Блок 4---------------- -->
+                    <div class="col-md-55">
+                        <div class="thumbnail">
+                            <div class="image view view-first">
+
+                            </div>
+                            <div class="caption">
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- --------------Блок 5---------------- -->
+                </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="col-md-3 col-xs-12">
-        <div class ="x_panel">
-            <div class="info">
-
-            </div>
-            <div class="button">
-
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-3 col-xs-12">
-        <div class ="x_panel">
-            <div class="info">
-
-            </div>
-            <div class="button">
-
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-3 col-xs-12">
-        <div class ="x_panel">
-            <div class="info">
-
-            </div>
-            <div class="button">
-
-            </div>
-        </div>
-    </div>
 
 </div>
